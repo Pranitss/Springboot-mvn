46722ff11562d9af564464084696f9f40af60e7f
pranit sarode <pranitsarode2003@gmail.com>
2024-05-09 18:29:29 +0530

    1 msg

---
+++ src/main/java/com/asterisc/in/pack/MyydayApplication.java
@@ -0,0 +1,13 @@
+package com.asterisc.in.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyydayApplication.class, args);
+	}
+
+}
